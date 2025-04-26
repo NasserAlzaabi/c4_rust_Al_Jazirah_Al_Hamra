e85eedba13a6c97825c1a873ee1ae883fd09d621
NasserAlzaabi <nasseralzaabi03@gmail.com>
2025-04-26 17:40:59 +0400

    VM + VM tests complete

--- target/debug/deps/c4-dcf7cd31cf8fd4f4.d
+++
@@ -1,8 +0,0 @@
-C:\Users\user\Desktop\Projects\c4_rust_Al_Jazirah_Al_Hamra\target\debug\deps\c4-dcf7cd31cf8fd4f4.exe: src\lib.rs src\vm.rs src\parser.rs src\lexer.rs
-
-C:\Users\user\Desktop\Projects\c4_rust_Al_Jazirah_Al_Hamra\target\debug\deps\c4-dcf7cd31cf8fd4f4.d: src\lib.rs src\vm.rs src\parser.rs src\lexer.rs
-
-src\lib.rs:
-src\vm.rs:
-src\parser.rs:
-src\lexer.rs:
--- target/debug/deps/tokenization-705d586716168167.d
+++
@@ -1,5 +0,0 @@
-C:\Users\user\Desktop\Projects\c4_rust_Al_Jazirah_Al_Hamra\target\debug\deps\tokenization-705d586716168167.exe: tests\tokenization.rs
-
-C:\Users\user\Desktop\Projects\c4_rust_Al_Jazirah_Al_Hamra\target\debug\deps\tokenization-705d586716168167.d: tests\tokenization.rs
-
-tests\tokenization.rs:
