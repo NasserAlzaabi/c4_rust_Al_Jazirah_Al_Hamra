f973e408af1b7458ad2ccba90d961736adffb11c
NasserAlzaabi <nasseralzaabi03@gmail.com>
2025-04-26 17:48:31 +0400

    merged VM with main branch, no conflicts, moved lexer tests to test folder

--- target/debug/deps/c4_rust_Al_Jazirah_Al_Hamra-52da0130ecab5f0b.d
+++
@@ -1,6 +0,0 @@
-C:\Users\user\Desktop\Projects\c4_rust_Al_Jazirah_Al_Hamra\target\debug\deps\c4_rust_Al_Jazirah_Al_Hamra-52da0130ecab5f0b.exe: src\c4.rs src\vm.rs
-
-C:\Users\user\Desktop\Projects\c4_rust_Al_Jazirah_Al_Hamra\target\debug\deps\c4_rust_Al_Jazirah_Al_Hamra-52da0130ecab5f0b.d: src\c4.rs src\vm.rs
-
-src\c4.rs:
-src\vm.rs:
