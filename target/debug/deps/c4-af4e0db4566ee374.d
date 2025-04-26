20d118410dc075b67e317875d8e7f6619e3025a1
NasserAlzaabi <nasseralzaabi03@gmail.com>
2025-04-26 17:40:40 +0400

    VM + VM tests complete

---
+++ target/debug/deps/c4-af4e0db4566ee374.d
@@ -0,0 +1,10 @@
+C:\Users\user\Desktop\Projects\c4_rust_Al_Jazirah_Al_Hamra\target\debug\deps\libc4-af4e0db4566ee374.rmeta: src\lib.rs src\vm.rs src\parser.rs src\lexer.rs
+
+C:\Users\user\Desktop\Projects\c4_rust_Al_Jazirah_Al_Hamra\target\debug\deps\libc4-af4e0db4566ee374.rlib: src\lib.rs src\vm.rs src\parser.rs src\lexer.rs
+
+C:\Users\user\Desktop\Projects\c4_rust_Al_Jazirah_Al_Hamra\target\debug\deps\c4-af4e0db4566ee374.d: src\lib.rs src\vm.rs src\parser.rs src\lexer.rs
+
+src\lib.rs:
+src\vm.rs:
+src\parser.rs:
+src\lexer.rs:
